7edc4f3031b3b8fffbaf887b3bef766b6bfe63ce
vishek.patel <vishek.patel@moveinsync.com>
2023-07-08 15:53:27 +0530

    fixed issue

--- pages/Modal2.js
+++ pages/Modal2.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import styles from '@/styles/Modal.module.css';
 
 const Modal = ({ children, onClose }) => {
+  if (typeof window === 'undefined') {
+    return null; // Render nothing during server-side rendering
+  }
+
   return ReactDOM.createPortal(
     <>
-      <div className={styles['modal-overlay1']} />
-      <div className={styles['modal-content1']}>
-     
-     
-        <button className={styles['modal-close1']} onClick={onClose}>
+      <div className={styles['modal-overlay']} />
+      <div className={styles['modal-content']}>
+        <button className={styles['modal-close']} onClick={onClose}>
           X
         </button>
         {children}
